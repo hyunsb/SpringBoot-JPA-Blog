0d32d0a8b0fca755aee70e3e43e7e371a0bd8b40
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-23 15:09:16 +0900

    feat: blog project start ;

---
+++ blog/src/main/java/com/hyunsb/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.hyunsb.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
