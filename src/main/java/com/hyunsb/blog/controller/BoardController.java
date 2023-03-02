d9c8d8fd8a52fe544ee984ff271cdd5f30c26ba9
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-03-02 18:01:51 +0900

    feat: 글 목록 불러오기 기능 업데이트

--- src/main/java/com/hyunsb/blog/controller/BoardController.java
+++ src/main/java/com/hyunsb/blog/controller/BoardController.java
@@ -1,19 +1,26 @@
 package com.hyunsb.blog.controller;
 
 import com.hyunsb.blog.config.auth.PrincipalDetail;
+import com.hyunsb.blog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping({"","/"})
-    public String index(){
+    public String index(Model model){
 //        @AuthenticationPrincipal PrincipalDetail principal  : parameter
 //        System.out.println("로그인 사용자 아이디: " + principal.getUsername());
-        return "index";
+
+        model.addAttribute("boards", boardService.findAll());
+        return "index"; // viewResolver 작동
     }
 
     @GetMapping("/board/saveForm")
